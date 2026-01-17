d2f7061e81060c728391be2d484f3f23d3b731c0
Jagraj Bains <jagrajbains@Jagrajs-MacBook-Pro.local>
2026-01-17 14:17:42 +0530

    feat: NextAuth frontend setup

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,23 +3,22 @@ import Footer from "@/components/base/Footer";
 import HeroSection from "@/components/base/HeroSection";
 import Navbar from "@/components/base/Navbar";
 import UserReviews from "@/components/base/UserReviews";
-import Image from "next/image";
+import { getServerSession } from "next-auth";
 
-export default function Home() {
+import { authOptions, CustomSession } from "./api/auth/[...nextauth]/options";
+
+export default async function Home() {
+  const session: CustomSession | null = await getServerSession(authOptions); 
   return (
 		<div className="min-h-screen flex flex-col ">
 			{/* Header */}
-			{/* <Navbar user={session?.user ?? null} /> */}
-			<Navbar />
+			<Navbar user={session?.user ?? null} />
 			{/* Hero Section */}
 			<HeroSection />
-
 			{/* Features Section */}
 			<FeatureSection />
-
 			{/* User Reviews Section */}
 			<UserReviews />
-
 			{/* Footer */}
 			<Footer />
 		</div>
